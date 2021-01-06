49dce5ee8ab7b1f79113e3054a3ad7327e0cc090
Deo <lukyanenko88@gmail.com>
2021-01-07 00:52:51 +0200

    save employee added

--- src/main/java/com/deo/thymeleafdbdemo/dao/EmployeeRepository.java
+++ src/main/java/com/deo/thymeleafdbdemo/dao/EmployeeRepository.java
@@ -7,7 +7,14 @@ import com.deo.thymeleafdbdemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+
 @RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //thats all ...no need to write any code!!!
+
+    //add a method to sort by last name
+
+
+    public List<Employee> findAllByOrderByLastNameAsc();
 }
